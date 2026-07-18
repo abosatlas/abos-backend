2d4d3d7c78f973a56c6e5103849725e0c2b00e45
abosatlas <abosatlas@gmail.com>
2026-07-18 14:17:55 +0300

    Add transaction block in 40_sales.sql
    Added transaction block for sales-related tables.

--- database/40_sales.sql
+++ database/40_sales.sql
@@ -1,1 +1,17 @@
+BEGIN;
 
+-- Sales Tables
+
+-- product_categories
+-- products
+-- price_lists
+-- price_list_items
+-- quotations
+-- quotation_items
+-- sales_orders
+-- sales_order_items
+-- invoices
+-- invoice_items
+-- payments
+
+COMMIT;
