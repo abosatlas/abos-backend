61c07fed30ccc8b4365e5f6e3f3e2b316948ee29
abosatlas <abosatlas@gmail.com>
2026-07-18 13:55:52 +0300

    Add create_updated_at_trigger function
    Added a function to create or replace update triggers for specified tables.

--- database/03_triggers.sql
+++ database/03_triggers.sql
@@ -1,1 +1,26 @@
+-- ==========================================================
+-- ABOS ERP v1
+-- Common Triggers
+-- ==========================================================
 
+CREATE OR REPLACE FUNCTION create_updated_at_trigger(table_name TEXT)
+RETURNS VOID
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+
+EXECUTE format('
+DROP TRIGGER IF EXISTS trg_update_%I ON %I;
+',table_name,table_name);
+
+EXECUTE format('
+CREATE TRIGGER trg_update_%I
+BEFORE UPDATE
+ON %I
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+',table_name,table_name);
+
+END;
+$$;
