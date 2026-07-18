3f777cd35c301fd9d31d0ac2fbb30f572f05d399
abosatlas <abosatlas@gmail.com>
2026-07-18 13:56:38 +0300

    Update 96_rls.sql

--- database/96_rls.sql
+++ database/96_rls.sql
@@ -1,1 +1,13 @@
+-- ==========================================================
+-- ABOS ERP v1
+-- Row Level Security
+-- ==========================================================
 
+ALTER TABLE companies ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY company_policy
+ON companies
+FOR ALL
+USING (
+id=current_company()
+);
