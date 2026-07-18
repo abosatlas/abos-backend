576011c4b0833f705d6d88ee9cd1ed451f5d6a9f
abosatlas <abosatlas@gmail.com>
2026-07-18 14:37:44 +0300

    Add inventory validation trigger

--- database/03_triggers.sql
+++ database/03_triggers.sql
@@ -24,3 +24,37 @@ EXECUTE FUNCTION update_updated_at();
 
 END;
 $$;
+CREATE OR REPLACE FUNCTION prevent_negative_quantity()
+
+RETURNS TRIGGER
+
+LANGUAGE plpgsql
+
+AS $$
+
+BEGIN
+
+IF NEW.quantity < 0 THEN
+
+RAISE EXCEPTION 'Quantity cannot be negative';
+
+END IF;
+
+RETURN NEW;
+
+END;
+
+$$;
+
+DROP TRIGGER IF EXISTS trg_inventory_quantity
+ON inventory_transactions;
+
+CREATE TRIGGER trg_inventory_quantity
+
+BEFORE INSERT OR UPDATE
+
+ON inventory_transactions
+
+FOR EACH ROW
+
+EXECUTE FUNCTION prevent_negative_quantity();
