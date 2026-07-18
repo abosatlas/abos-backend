a01ca323dccd126c4de1d61daf2a7906c8fc0196
abosatlas <abosatlas@gmail.com>
2026-07-18 13:58:21 +0300

    Add additional SQL script includes to database

--- database/99_abos_database_v1.sql
+++ database/99_abos_database_v1.sql
@@ -1,1 +1,18 @@
+\i 00_extensions.sql
+\i 01_types.sql
+\i 02_functions.sql
+\i 03_triggers.sql
 
+\i 10_core.sql
+\i 20_hr.sql
+\i 30_crm.sql
+\i 40_sales.sql
+\i 50_inventory.sql
+\i 60_purchasing.sql
+\i 70_finance.sql
+\i 80_projects.sql
+\i 90_assets.sql
+
+\i 95_views.sql
+\i 96_rls.sql
+\i 97_seed.sql
