fc56738643d66a58463963eab5f2a3ba6bd3f090
abosatlas <abosatlas@gmail.com>
2026-07-18 13:56:16 +0300

    Update 95_views.sql

--- database/95_views.sql
+++ database/95_views.sql
@@ -1,1 +1,19 @@
+-- ==========================================================
+-- ABOS ERP v1
+-- Views
+-- ==========================================================
 
+CREATE OR REPLACE VIEW active_companies AS
+SELECT *
+FROM companies
+WHERE status='active';
+
+CREATE OR REPLACE VIEW active_customers AS
+SELECT *
+FROM customers
+WHERE status='active';
+
+CREATE OR REPLACE VIEW active_suppliers AS
+SELECT *
+FROM suppliers
+WHERE status='active';
