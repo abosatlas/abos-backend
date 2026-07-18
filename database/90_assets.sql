d458c40e5255c23d3c99ef692759b35bd0fab1c5
abosatlas <abosatlas@gmail.com>
2026-07-18 13:59:45 +0300

    Update 90_assets.sql

--- database/90_assets.sql
+++ database/90_assets.sql
@@ -1,1 +1,14 @@
+-- ==========================================================
+-- ABOS ERP v1
+-- Assets Module
+-- ==========================================================
 
+BEGIN;
+
+-- Asset Categories
+-- Assets
+-- Asset Movements
+-- Asset Maintenance
+-- Asset Depreciation
+
+COMMIT;
