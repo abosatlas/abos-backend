7c17864015ce89600890c5710e5c190561cb918b
abosatlas <abosatlas@gmail.com>
2026-07-18 13:57:58 +0300

    Add seed data for Demo Company in 97_seed.sql

--- database/97_seed.sql
+++ database/97_seed.sql
@@ -1,1 +1,21 @@
+-- ==========================================================
+-- Seed Data
+-- ==========================================================
 
+INSERT INTO companies
+(
+id,
+name,
+status,
+created_at,
+updated_at
+)
+VALUES
+(
+gen_random_uuid(),
+'Demo Company',
+'active',
+NOW(),
+NOW()
+)
+ON CONFLICT DO NOTHING;
